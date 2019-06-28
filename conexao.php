83a2dbd5d39eb4d34332a431fdf23c8be9bcdf63
Thiago <bthiagos@gmail.com>
2019-06-27 22:24:22 -0300

    Arquivos do Sistema

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
-define('HOST', '127.0.0.1');
+define('HOST', 'localhost');
 define('USUARIO', 'root');
-define('SENHA', 'a7v8xx');
+define('SENHA', 'senha');
 define('DB', 'defensoria');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
