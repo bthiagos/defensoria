5d380134cff95774ad8df8d697a3bb781afb7010
Thiago <bthiagos@gmail.com>
2019-06-28 11:32:31 -0300

    Alteração do Sistema

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 define('HOST', 'localhost');
 define('USUARIO', 'root');
 define('SENHA', '');
-define('DB', 'defensoria');
+define('DB', 'db_defensoria');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
 ?>
